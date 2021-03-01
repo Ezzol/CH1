76db49ba62a35bd548d6db92c42a45e692ac31f2
Ezzol <18054161@student.hhs.nl>
2021-03-01 18:23:25 +0100

    Raket JS update
    Raket wordt nu vanuit JavaScript verplaatst. De dag bepaalt waar de raket staat op de lijn.

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
+// Data om de raket te laten bewegen
+// variabelen van begindag, de dag van vandaag en het totaal aantal dagen dat de raket er over doet
+const dagenTotaal = 240
+const beginDag = new Date("01/01/2021") 
+let vandaag = new Date()
+
+// Dit berekent het aantal dagen tussen de begindag en vandaag
+let miliseconden = vandaag.getTime() - beginDag.getTime()
+let dagenOnderweg = Math.round(miliseconden / (1000 * 3600 * 24))
+
+// Geeft het percentage aan hoe ver je al onderweg bent. Bepaalt waar je op de lijn moet zitten 
+var percentageAfgelegd = dagenOnderweg / dagenTotaal * 100;
+// Verandert de breedte van de container
+document.getElementById('rocketWrapper').style.width = percentageAfgelegd + '%';
+
+// Haalt de dag op en toont die in de hovertekst
+document.getElementById('vandaagDeDag').innerHTML = dagenOnderweg;
+
+
+
+
+
+
+// Chart voor eten
 var ctx = document.getElementById('voedselVoorraadChart').getContext('2d');
 var chart = new Chart(ctx, {
     // The type of chart we want to create
@@ -66,7 +90,7 @@ var chart = new Chart(ctx, {
 //     options: {
 //         responsive: true,
 //       legend: {
-//         display: false
+//         display: true
 //       }
 //     }
 //   });
@@ -84,7 +108,7 @@ var chart = new Chart(ctx, {
   
 //       var text = "3500",
 //           textX = Math.round((width - ctx.measureText(text).width) / 2),
-//           textY = height / 2;
+//           textY = height / 1.85;
   
 //       ctx.fillText(text, textX, textY);
 //       ctx.save();
@@ -93,7 +117,9 @@ var chart = new Chart(ctx, {
 
 
 
+  
 
+// De functie die het aantal mensen laat zien 
 
   var ctx2 = document.getElementById('aantalMensenChart').getContext('2d');
 var chart2 = new Chart(ctx2, {
