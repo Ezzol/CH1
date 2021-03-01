46b7fe16b87825975e4c42e20aa29da2174a08f1
Ezzol <18054161@student.hhs.nl>
2021-03-01 19:52:01 +0100

    Kleine aanpassingen

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 // Data om de raket te laten bewegen
-// variabelen van begindag, de dag van vandaag en het totaal aantal dagen dat de raket er over doet
-const dagenTotaal = 240
-const beginDag = new Date("01/01/2021") 
-let vandaag = new Date()
+// 3 variabelen voor het totaal aantal dagen dat de raket er over doet, de startdatum van vertrek en de datum van vandaag
+var daysTotal = 240;
+var startDate = new Date("01/01/2021");
+var today = new Date();
 
-// Dit berekent het aantal dagen tussen de begindag en vandaag
-let miliseconden = vandaag.getTime() - beginDag.getTime()
-let dagenOnderweg = Math.round(miliseconden / (1000 * 3600 * 24))
+// Dit berekent het aantal dagen tussen de begindag en vandaag. Math.round om de dag af te ronden naar een heel getal, want bijv. dag 1,25 bestaat niet
+var milSec = today.getTime() - startDate.getTime();
+var daysOnTheMove = Math.round(milSec / (1000 * 3600 * 24));
 
 // Geeft het percentage aan hoe ver je al onderweg bent. Bepaalt waar je op de lijn moet zitten 
-var percentageAfgelegd = dagenOnderweg / dagenTotaal * 100;
+var percentageDone = daysOnTheMove / daysTotal * 100;
 // Verandert de breedte van de container
-document.getElementById('rocketWrapper').style.width = percentageAfgelegd + '%';
+document.getElementById('rocketWrapper').style.width = percentageDone + '%';
 
 // Haalt de dag op en toont die in de hovertekst
-document.getElementById('vandaagDeDag').innerHTML = dagenOnderweg;
+document.getElementById('vandaagDeDag').innerHTML = daysOnTheMove;
 
 
 
@@ -62,62 +62,7 @@ var chart = new Chart(ctx, {
 
 // link naar code: https://jsfiddle.net/cmyker/ooxdL2vj/
 
-// var data = {
-//     labels: [
-//       "Red",
-//       "Blue",
-//       "Yellow"
-//     ],
-//     datasets: [
-//       {
-//         data: [40, 20, 40],
-//         backgroundColor: [
-//           "#FF6384",
-//           "#36A2EB",
-//           "#FFCE56"
-//         ],
-//         hoverBackgroundColor: [
-//           "#FF6200",
-//           "#36A2EB",
-//           "#FFCE56"
-//         ]
-//       }]
-//   };
-  
-//   var promisedDeliveryChart = new Chart(document.getElementById('voedselVoorraadChart'), {
-//     type: 'doughnut',
-//     data: data,
-//     options: {
-//         responsive: true,
-//       legend: {
-//         display: true
-//       }
-//     }
-//   });
-  
-//   Chart.pluginService.register({
-//     beforeDraw: function(chart) {
-//       var width = chart.chart.width,
-//           height = chart.chart.height,
-//           ctx = chart.chart.ctx;
-  
-//       ctx.restore();
-//       var fontSize = (height / 114).toFixed(2);
-//       ctx.font = fontSize + "em sans-serif";
-//       ctx.textBaseline = "middle";
-  
-//       var text = "3500",
-//           textX = Math.round((width - ctx.measureText(text).width) / 2),
-//           textY = height / 1.85;
-  
-//       ctx.fillText(text, textX, textY);
-//       ctx.save();
-//     }
-//   });
 
-
-
-  
 
 // De functie die het aantal mensen laat zien 
 
